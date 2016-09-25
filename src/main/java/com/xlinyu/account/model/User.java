aba964c32b535f5d5f8ac93557c8f778bb6512d3
ncc0706 <ncc0706@gmail.com>
2016-09-25 22:02:19 +0800

    修改用户model

--- src/main/java/com/xlinyu/account/model/User.java
+++ src/main/java/com/xlinyu/account/model/User.java
@@ -1,14 +1,23 @@
 package com.xlinyu.account.model;
 
+import org.mybatis.extension.auto.annotation.Column;
+import org.mybatis.extension.auto.annotation.Id;
+import org.mybatis.extension.auto.annotation.Table;
+
 /**
  * Created by gavin on 20/09/2016.
  */
+@Table(tableName = "sys_user", comment = "用户表")
 public class User {
 
+    @Column(columnName = "id", nullable = false, comment = "主键id")
+    @Id
     private String id;
 
+    @Column(columnName = "usernane", comment = "用户名")
     private String username;
 
+    @Column(columnName = "password", comment = "用户密码")
     private String password;
 
     public String getId() {
