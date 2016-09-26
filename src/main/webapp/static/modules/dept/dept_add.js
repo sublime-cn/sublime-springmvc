d86dfae05ffd105e419cfacc9887664acd9fc7bf
ncc0706 <ncc0706@gmail.com>
2016-09-26 14:03:48 +0800

    存在pjax不能加载script脚本问题

--- src/main/webapp/static/modules/dept/dept_add.js
+++ src/main/webapp/static/modules/dept/dept_add.js
@@ -2,27 +2,10 @@
  * Created by gavin on 25/09/2016.
  */
 $(function () {
-alert('olllllll');
-    initTreeView();
 
+    alert('olllllll');
+    //initTreeView();
 
 })
 
 
-function initTreeView() {
-    alert("ent");
-    $("#jsTreeParentUnit").jstree({
-        plugins: ["wholerow", "json_data"],
-        core: {
-            data: {
-                dataType: "json",
-                url: function (node) {
-                    return node.id === "#" ? "${base}/dept/tree/''" : "${base}/dept/tree/" + node.id
-                }
-            },
-            multiple: false
-        }
-    }).on("dblclick.jstree", function (node) {
-        selectParentMenu();
-    });
-}
